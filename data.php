5383aa4c317eb340bb7758c55afebed6a3d8d69b
Geno <geno@fireorbit.de>
2015-05-27 21:33:29 +0200

    Updates

--- data.php
+++ data.php
@@ -1,18 +1,52 @@
 <?php
 /**
- * DECLARE CACHE
+ * Settings
  */
 $DEV = true;
-$CACHE_PATH = '/tmp/breminale_ffhb_de.cache';
-$CACHE = json_decode(file_get_contents($CACHE_PATH), true);
 $CACHE_UPDATE_TIME = 10;
+$CACHE_PATH = '/tmp/breminale_ffhb_de.cache';
+$FB_PAGE='TravelOK';
+
+$FB_URL = 'graph.facebook.com/v2.3/'.$FB_PAGE.'/feed';
+$FB_APP_ID = '378325892362983';
+$FB_APP_Secret = '00e8ad973e1599c013e7728ed749bbe2';
+$FB_Token = '?fields=message'.
+	'&limit=250'.
+	'&since=1420070400'.
+	'&access_token='.$FB_APP_ID.'|'.$FB_APP_Secret;
+/**
+ * DECLARE CACHE
+ */
+$CACHE = json_decode(file_get_contents($CACHE_PATH), true);
 if(($CACHE['lastUpdate']+$CACHE_UPDATE_TIME) <= (time())){
 	$CACHE['change'] = true;
 	/**
 	 * UPDATE CACHE:
 	 */
 	$CACHE['Iterator']++;
+	$CACHE['feed'] = array();
+	$curl = curl_init();
+	curl_setopt($curl,CURLOPT_URL,'https://'.$FB_URL.$FB_Token);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($curl, CURLOPT_BINARYTRANSFER, true);
+	$request = json_decode(curl_exec($curl),true);
+	curl_close($curl);
+	$data = $request['data'];
+	foreach ($data as $item) {
+		preg_match_all('/#(\w*[a-zA-Z_]+\w*)/',$item['message'], $matches, PREG_OFFSET_CAPTURE);
+		if(count($matches[1]) > 0 ){
+			$hashtags = $matches[1];
+			$tags = array();
+			foreach ($hashtags as $tag) {
+				$tags[] = $tag[0];
+			}
+			$item['hashtags'] = $tags;
+		}
+		$CACHE['feed'][]=$item;
+	}
+
 }
+
 
 
 /**
@@ -21,13 +55,14 @@ if(($CACHE['lastUpdate']+$CACHE_UPDATE_TIME) <= (time())){
 $OUTPUT = array();
 //DEV OUTPUT
 if($DEV){
-	$OUTPUT[time] = time();
-	$OUTPUT[lastUpdate] = $CACHE['lastUpdate'];
+	$OUTPUT['time'] = time();
+	$OUTPUT['lastUpdate'] = $CACHE['lastUpdate'];
 }
 /**
  * Convert:
  */
-$OUTPUT[Iterator] = $CACHE['Iterator'];
+$OUTPUT['Iterator'] = $CACHE['Iterator'];
+$OUTPUT['feed'] = $CACHE['feed'];
 
 /**
  * SAVE CACHE
