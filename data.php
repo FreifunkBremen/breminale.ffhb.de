b370cedcf4b525ae739c6d9f48725cde47d1ea92
Geno <geno@fireorbit.de>
2015-06-26 18:45:50 +0200

    Updates

--- data.php
+++ data.php
@@ -12,7 +12,7 @@ $FB_APP_ID = '378325892362983';
 $FB_APP_Secret = '00e8ad973e1599c013e7728ed749bbe2';
 $FB_Token = '?fields=message'.
 	'&limit=250'.
-	'&since=1420070400'.
+	'&since=1415704271'.
 	'&access_token='.$FB_APP_ID.'|'.$FB_APP_Secret;
 /**
  * DECLARE CACHE
