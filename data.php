fa3ae90c18ad688162414600cc47e2d8c753b0db
Geno <geno@fireorbit.de>
2015-07-16 16:06:56 +0200

    Updates

--- data.php
+++ data.php
@@ -30,6 +30,8 @@ if(($CACHE['lastUpdate']+$CACHE_UPDATE_TIME) <= (time()*1000)){
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($curl, CURLOPT_BINARYTRANSFER, true);
 	$request = json_decode(curl_exec($curl),true);
+	curl_setopt($curl,CURLOPT_URL,'http://monitoring.breminale.digineo.de/json/statistic');
+	$statistic = json_decode(curl_exec($curl),true);
 	curl_close($curl);
 	$data = $request['data'];
 	foreach ($data as $item) {
@@ -47,6 +49,7 @@ if(($CACHE['lastUpdate']+$CACHE_UPDATE_TIME) <= (time()*1000)){
 			$CACHE['feed'][]=$item;
 		}
 	}
+	$CACHE['statistic']=$statistic;
 	$CACHE['lastUpdate'] = time()*1000;
 	$CACHE['today'] = mktime(0, 0, 0, date("m")  , date("d"), date("Y"))*1000;
 }
@@ -62,6 +65,7 @@ $OUTPUT['lastUpdate'] = $CACHE['lastUpdate'];
 $OUTPUT['Iterator'] = $CACHE['Iterator'];
 $OUTPUT['today'] = $CACHE['today'];
 $OUTPUT['feed'] = $CACHE['feed'];
+$OUTPUT['statistic'] = $CACHE['statistic'];
 if($DEV){
 	$OUTPUT['feed'][] = array(
 		'id' => 'i348586768525834_959578534093318',
